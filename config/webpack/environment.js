bef507804e0e7d8f380a3c5e626087187ede722d
shisongsong <shi1400875096@icloud.com>
2019-01-25 17:23:25 +0800

    webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,30 +1,3 @@
-const webpack = require('webpack');
-const { environment } = require('@rails/webpacker');
-const erb = require('./loaders/erb')
+const { environment } = require('@rails/webpacker')
 
-// Add an additional plugin of your choosing : ProvidePlugin
-environment.plugins.prepend(
-    'Provide',
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      jquery: 'jquery',
-    })
-);
-
-// Add an additional plugin of your choosing : IgnorePlugin
-environment.plugins.insert(
-    'IgnorePlugin',
-    new webpack.IgnorePlugin(/^\.\/node_modules/, /turbolinks/, /bootstrap/)
-);
-
-// resolve-url-loader must be used before sass-loader
-environment.loaders.get('sass').use.splice(-1, 0, {
-  loader: 'resolve-url-loader',
-  options: {
-    attempts: 1
-  }
-});
-
-environment.loaders.append('erb', erb)
-module.exports = environment;
+module.exports = environment
